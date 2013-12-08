5ea9c4b3d85dccfa953d972310a37fb4963b7c44
daniel <daniel5hbs@gmail.com>
2013-12-08 10:50:01 +0800

    add string replace

---
+++ src/main/java/practice/SelfTest/SimpleTest.java
@@ -0,0 +1,47 @@
+package practice.SelfTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import practice.algorithm.arrayAndString.StringReplace;
+
+/**
+ * User: Daniel
+ * Date: 13-12-7
+ * Time: 下午9:27
+ */
+public class SimpleTest {
+	private static Logger logger = LoggerFactory.getLogger(SimpleTest.class);
+
+	/**
+	 * test for slf4j and log4j
+	 */
+	private static void slf4jTest() {
+		Map<String, Object> nameMap = new HashMap<String, Object>();
+		nameMap.put("name", "daniel");
+		String name = "nkcoder";
+		logger.info("hello, {}", nameMap);
+
+	}
+
+	private static void stringReplaceTest(String src) {
+		String destByBuilder = StringReplace.replaceByBuilder(src);
+		String destByLibrary = StringReplace.replaceByLibrary(src);
+
+		logger.info("src: {}\n destByBuilder: {}\n destByLibrary: {}\n", src, destByBuilder, destByLibrary);
+	}
+
+
+	public static void main(String... args) {
+		SimpleTest.slf4jTest();
+
+		final String src = "The standard Java libraries fail to provide enough methods for " +
+				"manipulation of its core classes.";
+		SimpleTest.stringReplaceTest(src);
+	}
+
+
+
+}
---
+++ src/main/java/practice/algorithm/arrayAndString/StringReplace.java
@@ -0,0 +1,9 @@
+package practice.algorithm.arrayAndString;
+
+/**
+ * User: Daniel
+ * Date: 13-12-8
+ * Time: 上午9:39
+ */
+public class StringReplace {
+}
